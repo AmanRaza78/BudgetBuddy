c655022d2b1c22ddc48bc6e117b527a823d9e98c
AmanRaza78 <razakhanaman9@gmail.com>
2024-07-14 15:08:38 +0530

    basic authentication setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { SignIn } from "@/components/sign-in";
+
 export default function Home() {
   return (
-    <h1>Home Page</h1>    
+    <>
+    <h1>Home Page</h1>
+    <SignIn/> 
+    </>
   );
 }
